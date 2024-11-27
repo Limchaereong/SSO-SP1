1a0b0d847e747ba7db305bc4c021a1073e6467ee
임채령 <sa990422@gmail.com>
2024-11-27 15:28:56 +0900

    feat: 띄어쓰기 추가

--- src/main/java/com/example/demo/common/dto/response/TokenResponseDto.java
+++ src/main/java/com/example/demo/common/dto/response/TokenResponseDto.java
@@ -2,6 +2,6 @@ package com.example.demo.common.dto.response;
 
 public record TokenResponseDto (String accessToknen,
 								String idToken,
-								String refreshToken){
+								String refreshToken) {
 
 }
